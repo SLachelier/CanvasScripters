6cff32131aa0a4e04cc35fdbe7f8168f129127fb
Caleb Kruger <enderschosen@gmail.com>
2023-07-13 12:19:20 -0600

    added axios

--- my-electron-app/preload.js
+++ my-electron-app/preload.js
@@ -1,5 +1,19 @@
 const { contextBridge, ipcRenderer } = require('electron');
+// const axios = require('axios');
 
 contextBridge.exposeInMainWorld('eAPI', {
-    onUpdateCounter: (callback) => ipcRenderer.on('update-counter', callback)
+    onUpdateCounter: (callback) => ipcRenderer.on('update-counter', callback),
+    helloTest: (channel, data) => ipcMain.send(channel, data),
+
+});
+
+contextBridge.exposeInMainWorld('axios', {
+    get: async (data) => {
+        console.log('Stringified: ', data);
+        console.log('Parsed ', JSON.parse(data));
+        const result = await ipcRenderer.invoke('axios:get', data)
+        console.log(result);
+
+        return result;
+    }
 });
