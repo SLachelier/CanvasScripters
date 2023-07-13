0dc46fd630b60d07c59d9f75cbee75ca9e3b9c43
Caleb Kruger <enderschosen@gmail.com>
2023-07-13 12:18:52 -0600

    added axios

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -5,12 +5,14 @@ const {
     ipcMain,
     Menu
 } = require('electron');
+const axios = require('axios');
 
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1080,
         height: 720,
+        minWidth: 900,
         webPreferences: {
             nodeIntegration: true,
             preload: path.join(__dirname, './preload.js')
@@ -21,15 +23,39 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
+
+    ipcMain.handle('axios:get', async (event, searchData) => {
+        console.log(searchData);
+        const searchQuery = JSON.parse(searchData);
+        console.log(searchQuery.domain);
+        console.log(searchQuery.user_id);
+        console.log(searchQuery.token);
+        const url = `https://${searchQuery.domain}/api/v1/conversations?as_user_id=${searchQuery.user_id}`
+        console.log(url);
+        const conversations = await axios.get(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${searchQuery.token}`
+            }
+        });
+
+        const data = conversations.data;
+
+        console.log(data)
+        return data;
+    })
     createWindow();
 
     // for mac os creates new window when activated
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     })
+
+
 })
 
 // for windows and linux closes app when all windows are closed
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 })
+
