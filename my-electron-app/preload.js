1a797c8d61f8a161347daf30cac63b0e8e643fd3
Caleb Kruger <enderschosen@gmail.com>
2023-09-03 09:07:09 -0600

    added converation and assignment group stuff

--- my-electron-app/preload.js
+++ my-electron-app/preload.js
@@ -8,12 +8,35 @@ contextBridge.exposeInMainWorld('eAPI', {
 });
 
 contextBridge.exposeInMainWorld('axios', {
-    get: async (data) => {
-        console.log('Stringified: ', data);
-        console.log('Parsed ', JSON.parse(data));
-        const result = await ipcRenderer.invoke('axios:get', data)
-        console.log(result);
+    getConvos: async (data) => {
+        const result = await ipcRenderer.invoke('axios:getConvos', data);
+        console.log('in preload total result ', result.length);
 
         return result;
+    },
+    deleteConvos: async (data) => {
+        console.log('inside deleteConvos');
+
+        return await ipcRenderer.invoke('axios:deleteConvos', data);
+        // const result = await ipcRenderer.invoke('axios:deleteConvos', data, url);
+    },
+    getEmptyAssignmentGroups: async (data) => {
+        console.log('inside preload getEmptyAssignmentGroups');
+        return await ipcRenderer.invoke('axios:getEmptyAssignmentGroups', data);
+    },
+    deleteEmptyAssignmentGroups: async (data) => {
+        console.log('Inside axios:deleteEmptyAssignmentGroups');
+
+        return await ipcRenderer.invoke('axios:deleteEmptyAssignmentGroups', data);
+    }
+});
+
+contextBridge.exposeInMainWorld('csv', {
+    sendToCSV: async (data) => {
+        console.log('inside csv exporter');
+
+        //console.log(data);
+
+        await ipcRenderer.invoke('csv:sendToCSV', data);
     }
 });
