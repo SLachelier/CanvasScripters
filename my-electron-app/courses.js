b0d80280baea4cdac964a3df8fd43ce0cf89e53b
Caleb Kruger <enderschosen@gmail.com>
2024-10-21 22:06:03 -0600

    added createSupportCase

--- my-electron-app/courses.js
+++ my-electron-app/courses.js
@@ -26,6 +26,70 @@ async function resetCourse(data) {
     }
 }
 
+async function createSupportCourse(data) {
+    console.log('inside createSupportCourse');
+
+    let url = `${data.domain}/api/v1/accounts/self/courses`;
+
+    const courseData = {
+        course: {
+            name: data.course.name,
+            default_view: 'feed'
+        },
+        offer: data.course.publish
+    }
+
+    const axiosConfig = {
+        method: 'post',
+        url: url,
+        headers: {
+            'Authorization': `Bearer ${data.token}`
+        },
+        data: courseData
+    };
+
+    try {
+        const request = async () => {
+            return await axios(axiosConfig);
+        }
+        const response = await errorCheck(request);
+        return response.data;
+    } catch (error) {
+        throw error
+    }
+}
+
+async function editCourse(data) {
+    let url = `${data.domain}/api/v1/courses/${data.course_id}`;
+
+    const courseData = {
+        course: {
+            blueprint: true
+        }
+    }
+
+    const axiosConfig = {
+        method: 'put',
+        url: url,
+        headers: {
+            'Authorization': `Bearer ${data.token}`
+        },
+        data: courseData
+    };
+
+    try {
+        const request = async () => {
+            return await axios(axiosConfig);
+        }
+        const response = await errorCheck(request);
+        return response.data;
+    } catch (error) {
+        throw error
+    } finally {
+        console.log('Finished editing course');
+    }
+}
+
 module.exports = {
-    resetCourse
+    resetCourse, createSupportCourse, editCourse
 };
